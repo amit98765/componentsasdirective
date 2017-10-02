dbedeb545849fcdcab9a51be85a8b54f0bab61a3
amit98765 <amitkumarmittalakm@gmail.com>
2017-10-02 11:09:00 +1100

    working well

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  images: any[] = [
+    { src: "../assets/images/assignment2_2.png", title: "The assignment i did", description: "This version contains few mistakes" },
+    { src: "../assets/images/assignment2.png", title: "Assignment", description: "this version works well" },
+    { src: "../assets/images/ay0ol1.jpg" }
+  ]
 }
